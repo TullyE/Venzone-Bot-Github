e0fefcbc68d87f412179c555e94bc76f035cde8e
Tully Eva <TullyEva@icloud.com>
2021-06-18 16:11:43 -0700

    added Var: Mod sees if person allowed to send msgs

--- VenzoneFanartPurgeSrcCont.py
+++ VenzoneFanartPurgeSrcCont.py
@@ -25,6 +25,10 @@ async def on_message(message):
     Author = message.author
     Roles = discord.utils.get(Author.roles, name = 'Mods')
     Mod = 'Mods' == str(Roles)
+    for ids in allowed_ids: #if the person is allowed to type messages
+        if message.author.id == allowed_ids[ids]:
+            Mod = True
+            break
 
     if Channel == client.get_channel(835641585777639434): #If the message WAS sent in Fanart (The channel that's being purged)
         msg_id = await Channel.fetch_message(message.id)
@@ -32,7 +36,6 @@ async def on_message(message):
 
         if message.attachments or message.reference: #if it's a reply or an attachment >>>This is a boolean check<<<
             return
-
 
         if Mod or message.author == client.user: #if a mod types the message
             if message.content == '!Del':
@@ -42,13 +45,6 @@ async def on_message(message):
                 del_msg.clear()
                 await del_msg_chan.send(f'<#835641585777639434> has been purged by {message.author.mention}')
                 await message.delete()
-            return
-
-        for ids in allowed_ids: #if the person is allowed to type messages
-            if message.author.id == allowed_ids[ids]:
-                return
-
-        if message.author == client.user: #if it's this bot thats typing messages
             return
         
         del_msg.append(msg_id)
