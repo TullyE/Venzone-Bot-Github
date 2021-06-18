2e10797078a37ae0c334684e73b27e9905ee0ab2
Tully Eva <TullyEva@icloud.com>
2021-06-18 15:28:00 -0700

    V00

--- VenzoneFanartPurgeSrcCont.py
+++ VenzoneFanartPurgeSrcCont.py
@@ -1,35 +1,54 @@
 import discord 
 from Token import *
 from discord.ext import commands
-import asyncio, discord
+from datetime import datetime
 
+'''
+THE IS WORKING EXECPET FOR DELETING EVERY 24 HRS
+Added an edit to see if .gitignore worked Not being added though
+'''
 client = discord.Client()
-allowed_ids = {'3va':502253915463614477, 'Panda':635845590491725852, 'Cool Kid':744455901011902484}
+Channel_name = '💗-fanart'
+msg_to_delete = []
+Noon = 24*60
+allowed_ids = {'3va':502253915463614477} #'Panda':635845590491725852 | 'Cool Kid':744455901011902484
 del_msg = []
 
-async def my_background_task():
-    channel = client.get_channel(835641585777639434) #THE INT CAN BE CHANED THATS THE CHANNEL ID
-    while True:
-        await channel.send('!Del') #What you want to be sent 
-        await asyncio.sleep(86400) #every x sec
+
+
+
+
+
+
+def Time_To_Noon():
+    now = datetime.now()
+    Current_Time = now.strftime("%H:%M:%S")
+    Current_Time = Current_Time.split(':')
+    time_in_sec = (int(Current_Time[0]) * 60) + (int(Current_Time[1])) + int(Current_Time[2])/60
+    time_to_Noon = Noon - time_in_sec
+    return time_to_Noon
+
+bot_chan = client.get_channel(854812658985599006)
+bot_chan.send('test')
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-    await my_background_task()
 
 @client.event
 async def on_message(message):
-
     Channel = message.channel
     Author = message.author
     msg = await Channel.fetch_message(message.id)
-    bot_chan = client.get_channel(854812658985599006) #Channel where deleted messages are sent
+    bot_chan = client.get_channel(854812658985599006)
 
-    if Channel != client.get_channel(835641585777639434): #If the message wasn't sent in Fanart (The channel that's being purged)
+    if Channel != client.get_channel(835641585777639434):
         return
 
     if message.attachments or message.reference: #if it's a reply or an attachment
+        return
+
+    if message.author == client.user: #if it's this bot thats typing messages
         return
 
     Roles = discord.utils.get(Author.roles, name = 'Mods')
@@ -47,10 +66,17 @@ async def on_message(message):
     for ids in allowed_ids: #if the person is allowed to type messages
         if message.author.id == allowed_ids[ids]:
             return
-
-    if message.author == client.user: #if it's this bot thats typing messages
-        return
-    
     del_msg.append(msg)
+
+    time_to_next_delete = Time_To_Noon()*60
+
+    if message.attachments or message.reference: #if it's a reply or an attachment
+        return
+    try:
+        await message.delete(delay = time_to_next_delete)
+    except:
+        return
+
+
 
 client.run(Token)
